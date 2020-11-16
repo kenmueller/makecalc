ea7cdb990689fa6d4545d29630126fcfd53dca3a
kenmueller <kenmueller0@gmail.com>
2020-11-16 13:33:41 -0800

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+import styles from 'styles/Home.module.scss'
+
+const Home: NextPage = () => (
+	<div className={styles.root}>
+		<Head>
+			<title>Next.js</title>
+		</Head>
+		<h1>If you see this, your Next.js app is working!</h1>
+	</div>
+)
+
+export default Home
