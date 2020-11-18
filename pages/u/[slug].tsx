3e5296c4031e6b336177b79883bd6c114b384199
Ken Mueller <kenmueller0@gmail.com>
2020-11-17 17:18:17 -0800

    header

--- pages/u/[slug].tsx
+++ pages/u/[slug].tsx
@@ -6,6 +6,10 @@ import User from 'models/User'
 import Calculator from 'models/Calculator'
 import getUserWithSlug from 'lib/getUserWithSlug'
 import getCalculators from 'lib/getCalculators'
+import useCurrentUser from 'hooks/useCurrentUser'
+import SignOutButton from 'components/SignOutButton'
+
+import styles from 'styles/User.module.scss'
 
 interface UserPageProps {
 	user: User | null
@@ -16,11 +20,17 @@ const UserPage: NextPage<UserPageProps> = ({ user }) => {
 	if (!user)
 		return <NotFound />
 	
+	const currentUser = useCurrentUser()
+	
 	return (
 		<>
 			<Head>
 				<title key="title">{user.name} - makecalc</title>
 			</Head>
+			<header className={styles.header}>
+				<h1 className={styles.name}>{user.name}</h1>
+				{currentUser?.uid === user.id && <SignOutButton />}
+			</header>
 		</>
 	)
 }
