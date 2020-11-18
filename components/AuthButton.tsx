b36486130c470afbb487b3036ac6b824e6980a9a
Ken Mueller <kenmueller0@gmail.com>
2020-11-17 16:58:36 -0800

    links to page

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -1,45 +1,62 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect } from 'react'
+import Link from 'next/link'
 import { toast } from 'react-toastify'
-import cx from 'classnames'
 
 import authenticate from 'lib/authenticate'
-import unauthenticate from 'lib/unauthenticate'
+import getUser from 'lib/getUser'
 import useCurrentUser from 'hooks/useCurrentUser'
 
 import styles from 'styles/AuthButton.module.scss'
 
 const AuthButton = () => {
-	const [isLoading, setIsLoading] = useState(false)
 	const currentUser = useCurrentUser()
 	
+	const [slug, setSlug] = useState<string | null>(null)
+	const [isLoading, setIsLoading] = useState(false)
+	
 	const onClick = useCallback(async () => {
-		if (isLoading || currentUser === undefined)
+		if (isLoading || currentUser !== null)
 			return
 		
 		try {
 			setIsLoading(true)
-			await (currentUser ? unauthenticate : authenticate)()
+			await authenticate()
 		} catch ({ message }) {
 			toast.error(message)
 		} finally {
 			setIsLoading(false)
 		}
-	}, [isLoading, currentUser, setIsLoading])
+	}, [currentUser, isLoading, setIsLoading])
 	
-	return (
-		<button
-			className={cx(styles.root, {
-				[styles.authenticated]: currentUser
-			})}
-			disabled={isLoading || currentUser === undefined}
-			onClick={onClick}
-		>
-			{isLoading || currentUser === undefined
-				? 'loading...'
-				: currentUser?.displayName ?? 'sign in'
-			}
-		</button>
-	)
+	useEffect(() => {
+		if (!currentUser)
+			return setSlug(null)
+		
+		getUser(currentUser.uid)
+			.then(({ slug }) => setSlug(slug))
+			.catch(({ message }) => toast.error(message))
+	}, [currentUser, setSlug])
+	
+	return slug
+		? (
+			<Link href={`/u/${slug}`}>
+				<a className={styles.root}>
+					{currentUser?.displayName ?? 'anonymous'}
+				</a>
+			</Link>
+		)
+		: (
+			<button
+				className={styles.root}
+				disabled={isLoading || currentUser !== null}
+				onClick={onClick}
+			>
+				{isLoading || currentUser !== null
+					? 'loading...'
+					: 'sign in'
+				}
+			</button>
+		)
 }
 
 export default AuthButton
